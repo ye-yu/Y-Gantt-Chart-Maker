540f6df3f7254f3c1177dad54ccd82b1905ba827
avvcsdff <avvcsdff@users.noreply.github.com>
2018-06-03 15:26:09 +0800

    bluwerg

--- GCM.py
+++ GCM.py
@@ -1,11 +1,12 @@
 print("Running application")
+import functools
 print("Importing openpyxl")
 from openpyxl import Workbook, load_workbook
 print("Importing subprocess")
 import subprocess
 print("Importing sys")
 print("Importing PyQt5")
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QDate, QTime
 class FileIO():
 	def openfile(filename):
@@ -40,14 +41,24 @@ class FileIO():
 		sb.save('output.xlsm')
 class Tasks:
 	taskCount = 0
-	def __init__(self, dat = QDate(2001, 1, 1), ddu = 0, tms = QTime(0,0,0,0), tme = QTime(23, 59, 0,0), tan = "", pri = 0):
+	def __init__(self, dat, ddu, tms, tme, tan, pri):
 		Tasks.taskCount += 1
 		self.date = dat
 		self.dayDuration = ddu
 		self.timeStart = tms
 		self.timeEnd = tme
 		self.priority = pri
-		if tan == "":
+		if (dat == None):
+			self.date = QDate(2001, 1, 1)
+		if (ddu == None):
+			self.dayDuration = 0
+		if (tms == None):
+			self.timeStart = QTime(0,0,0,0)
+		if (tme == None):
+			self.timeEnd = QTime(23,59,0,0)
+		if (pri == None):
+			self.priority = 0
+		if (tan == ""):
 			self.taskName = "Task " + str(Tasks.taskCount)
 		else:
 			self.taskName = tan
@@ -66,24 +77,98 @@ class Tasks:
 		return self.taskName
 	def returnDayDuration(self):
 		return self.dayDuration
-class Applic(QWidget):
+class Applic(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		self.tasksList = []
+		self.eventName = "Untitled"
+		self.taskName = ""
 		self.__initUI__()
 
 	def __initUI__(self):
-		self.setGeometry(300,300,300,220)
-		self.setWindowTitle("Yosh")
+		self.setMenuBar()
+		QLabel("Task Name: ", self).move(25,25)
+		self.taskNameInput = QLineEdit(self)
+		self.taskNameInput.move(90,27)
+		self.taskNameInput.resize(450,25)
+		self.taskNameInput.textChanged.connect(self.varTaskName)
+		self.createTaskButton = self.createButton("Create New Task", 550,28, functools.partial(self.createTaskBtnClk))
+		self.statusBar().showMessage('Ready')
+
+		#main window configuration
+		self.resize(700,420)
+		qr = self.frameGeometry()
+		cp = QDesktopWidget().availableGeometry().center()
+		qr.moveCenter(cp)
+		self.move(qr.topLeft())
+		self.setWindowTitle("YSimple - " + self.eventName + ".xlsm")
 		self.show()
 
-	def createTask(self, dat = None, ddu = None, tms = None, tme = None, tan = None, pri = None):
-		if (dat == None or ddu == None or tms == None or tme == None or tan == None or pri == None):
-			print("Default value for the parameters is used.")
-			self.tasksList.append(Tasks())
+	def setMenuBar(self):
+		createNewEventAct = QAction("&New", self)
+		createNewEventAct.setShortcut("Ctrl+N")
+		createNewEventAct.setStatusTip("Create new event")
+		createNewEventAct.triggered.connect(self.createNewEventBtnClk)
+		
+		openEvent = QAction("&Open", self)
+		openEvent.setShortcut("Ctrl+O")
+		openEvent.setStatusTip("Open existing event")
+		openEvent.triggered.connect(self.empty)
+
+		saveEvent = QAction("&Save", self)
+		saveEvent.setShortcut("Ctrl+S")
+		saveEvent.setStatusTip("Save current event")
+		saveEvent.triggered.connect(self.commitWork)
+		
+		saveNewEvent = QAction("&Save as", self)
+		saveNewEvent.setShortcut("Ctrl+Shift+S")
+		saveNewEvent.setStatusTip("Save current event as new file")
+		saveNewEvent.triggered.connect(self.empty)
+
+		exitApp = QAction("&Save as", self)
+		exitApp.setShortcut("Ctrl+W")
+		exitApp.setStatusTip("Save current event as new file")
+		exitApp.triggered.connect(qApp.quit)
+		
+		menubar = self.menuBar()
+		fileMenu = menubar.addMenu("&File")
+		fileMenu.addAction(createNewEventAct)
+		fileMenu.addAction(openEvent)
+		fileMenu.addAction(saveEvent)
+		fileMenu.addAction(saveNewEvent)
+		fileMenu.addAction(exitApp)
+
+	def createNewEventBtnClk(self):
+		msgBox = QMessageBox()
+		msgBox.setIcon(QMessageBox.Question)
+		msgBox.setText("Current event is not empty.")
+		msgBox.setInformativeText("Continue?")
+		msgBox.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+		ret = msgBox.exec_()
+		if (ret == QMessageBox.Save):
+			print("Project saved.")
+		elif (ret == QMessageBox.Discard):
+			print("New project is created.")
 		else:
-			self.tasksList.append(Tasks(dat, ddu, tms, tme, tan, pri))
-		print("Task created: " + self.tasksList[len(self.tasksList) - 1].toString())
+			pass
+
+	def createTaskBtnClk(self):
+		self.createTask(None, None, None, None, self.taskName, None)
+		self.allEntryClear()
+
+	def allEntryClear(self):
+		self.taskNameInput.clear()
+
+	def varTaskName(self, text):
+		self.taskName = text
+		
+	def createTask(self, dat, ddu, tms, tme, tan, pri):
+		# if (dat == None or ddu == None or tms == None or tme == None or tan == None or pri == None):
+			# print("Default value for the parameters is used.")
+			# self.tasksList.append(Tasks())
+		# else:
+		self.tasksList.append(Tasks(dat, ddu, tms, tme, tan, pri))
+		self.statusBar().showMessage("Task created: " + self.tasksList[len(self.tasksList) - 1].toString())
 	
 	def deleteTask(self, taskNumber = None):
 		if (taskNumber == None):
@@ -98,12 +183,21 @@ class Applic(QWidget):
 	def commitWork(self):
 		FileIO.createBlank()
 		FileIO.insertValues(self.tasksList)
-if __name__ == '__main__':
-	app = QApplication([])
-	ex = Applic()
-	ex.createTask()
-	ex.createTask()
-	ex.createTask()
-	ex.commitWork()
-	app.exec_()
+	
+	def createButton(self, text, posx, posy, taskFunction):
+		btn = QPushButton(text, self)
+		btn.setToolTip(text)
+		btn.resize(btn.sizeHint())
+		btn.move(posx, posy)
+		btn.clicked.connect(taskFunction)
+		return btn
+	def empty(self):
+		print('Action triggered.')
+try:
+	if __name__ == '__main__':
+		app = QApplication([])
+		ex = Applic()
+		app.exec_()
+except:
+	print('Error')
 #FileIO.openfile("output.xlsm")
--- Resource/test.py
+++ Resource/test.py
@@ -1,24 +1,5 @@
-#to read from file
-from openpyxl import *
-sb = load_workbook('test.xlsm', read_only = False,  keep_vba = True)
-ss = sb['Sheet1']
-count = 2
-#print(ss['A' + str(count)].value)
-while(ss['A' + str(count)].value != None):
-	ss['A' + str(count)] = None
-	count += 1
-	if (count == 100):
-		print("Ended potentinally endless loop")
-sb.save('test2.xlsm')
-"""
-wb = Workbook()
-ss = sb['Sheet1']
-ws = wb.active
-ws['B1'] = "Start Time"
-ws['C1'] = "Duration"
-count = 2
-while(ws['A' + str(count)].value != None):
-	print(ws['A' + str(count)].value)
-	count += 1
-wb.save('test2.xlsm')
-"""
+def abc(a, b = 6, c):
+	print(a)
+	print(b)
+	
+abc(3,,3)
